8cbdf5d1360c5c2dd2eaf88bd308af120adf8293
cesargtz <cesareomargtz@gmail.com>
2020-12-17 11:02:31 -0700

    email and maps google fix

--- js/theme.js
+++ js/theme.js
@@ -3,7 +3,49 @@ Name: 			Theme Base
 Written by: 	Okler Themes - (http://www.okler.net)
 Theme Version:	8.0.0
 */
-
+document.addEventListener("DOMContentLoaded", function(event) {
+emailjs.init("user_VCV2fJ7SExXz708nP91Hr");
+document.getElementById("submit_btn1").addEventListener("click", () => {
+    var templateParams = {
+        name: document.getElementById("name1").value,
+        email: document.getElementById("email1").value,
+		message: document.getElementById("message1").value,
+		subject: document.getElementById("subject1").value,
+    };
+    document.querySelectorAll(".getin_form .form-control").forEach( (element) => {
+        element.style.border = '2px solid rgb(252,206,47)';
+    })
+    console.log(templateParams.name)
+    if (templateParams.name != '' && templateParams.email != '' && templateParams.message != '' )
+    {
+        document.querySelectorAll(".getin_form .form-control").forEach( (element) => {
+            element.style.border = '2px solid rgb(252,206,47)';
+        })
+        emailjs.send('service_9ixv46y', 'template_y8ce877', templateParams) //use your Service ID and Template ID
+            .then(function(response) {
+                document.querySelectorAll(".getin_form .form-control").forEach( (element) => {
+                    element.style.border = '2px solid rgb(173,200,40)';
+                })
+                console.log('Todo salio bien!', response.status, response.text);
+                Swal.fire(
+                    'Email enviado!',
+                    'En la brevedad nos estaremos contactando con usted. Muchas gracias!',
+                    'success')
+                document.getElementById("name1").value = ''
+                document.getElementById("email1").value = ''
+				document.getElementById("message1").value = ''
+				document.getElementById("subject1").value = ''
+            }, function(error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Algo salio mal!',
+                    footer: '<a href>Favor de volver a intentarlo</a>'
+                })
+            });
+        }
+    });
+});
 // Theme
 window.theme = {};
 
