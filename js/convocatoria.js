9793a4e5433658e8ccf3d97ec698bf7d08d417d3
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-04-16 10:24:43 -0600

    abrir convocatoria con enlace

--- js/convocatoria.js
+++ js/convocatoria.js
@@ -1,4 +1,3 @@
 function descargar() {
-    console.log('entro')
-    alert('descargar pdf')
+    window.open('https://www.dropbox.com/s/yzqzf5th07na2qn/Acta%20de%20la%20Junta%20Ordinaria%20del%20Consejo%20Directivo%20-%20abril.pdf?dl=0')
 }
